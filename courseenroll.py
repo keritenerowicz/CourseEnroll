5e27651ada4909c8f8104f623f4d58ade3ebc4e8
keritenerowicz <keri.tenerowicz@gmail.com>
2021-08-30 09:36:59 -0400

    refactoring

--- courseenroll.py
+++ courseenroll.py
@@ -1,7 +1,7 @@
-from datetime import datetime
 import time
-import webbrowser
+from datetime import datetime
 
+import webbrowser
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
@@ -29,18 +29,20 @@ class Window(webdriver.Chrome):
     textClass = 'SSR_REGFORM_VW$srt6$0'
     textSelectTerm = 'win0divSSR_DUMMY_RECV1GP$0'
     textAddClasses = 'DERIVED_REGFRM1_SS_TRANSACT_TITLE'
+    textError = 'DERIVED_SASSMSG_ERROR_TEXT$0'
 
 
     def __init__(self):
         super(Window, self).__init__()
-        self.timeout = 500
+        self.timeout = 400
         self.timeoutUser = 86400
-        self.loaded = False
+        self.timeoutLag = .5
+        self.timeout0 = .025
         self.startTime = '09:00:00'  # 9 am EST
         self.isSuccessful = 'notYet'
         self.toaster = ToastNotifier()
         self.login()
-        self.term()
+        #self.term()
         self.checkTime()
         self.waitEnrOpen()
         self.enrCheck()
@@ -50,10 +52,15 @@ class Window(webdriver.Chrome):
     def login(self):
         """Opens Student Center webpage and wait for user to log in."""
         self.get('https://studentcenter.cornell.edu')
+        """
+        actions = ActionChains(self)
+        actions.send_keys('net123' + Keys.TAB + 'password' + Keys.ENTER)
+        actions.perform()
+        """
         self.toaster.show_toast('Login',
-                                'Please log in through the opened Chrome window.',
-                                icon_path='images\logo.ico',
-                                duration=10)
+                                   'Please log in through the opened Chrome window.',
+                                   icon_path='images\logo.ico',
+                                   duration=10)
         while True:
             try:
                 self.switch_to_frame('ptifrmtgtframe')
@@ -84,37 +91,26 @@ class Window(webdriver.Chrome):
 
     def waitEnrOpen(self):
         """Checks cart is not empty, then begin enroll cycle."""
-        try:
-            self.loadPage()
-            self.elementPresentWait(Window.textClass, self.timeout)
-        except:
-            return
-
-        # Attempts cycle until time period opens, the finish first cycle through enrollment screens.
         self.loadPage()
-        self.click(Window.buttonStep2of3, self.timeoutUser)
-        WebDriverWait(self, self.timeout).until(EC.presence_of_element_located((By.ID, Window.textAddClasses)))
-        self.loadPage()
-        while self.elementPresentWait(Window.buttonStep2of3, .15):
-            self.click(Window.buttonStep2of3, self.timeoutUser)
-            self.loadPage()
-            if self.elementPresentWait(Window.buttonFinishEnrolling, 0.2):
+        self.elementPresentWait(Window.textClass, self.timeout)
+        while True:
+            try:
+                self.loadPage()
+                self.click(Window.buttonStep2of3, 0)
+                continue
+            except:
+                self.loadPage()
+                self.click(Window.buttonFinishEnrolling, self.timeout)
+                self.loadPage()
+                self.click(Window.buttonAddAnotherClass, self.timeout)
                 break
-            WebDriverWait(self, self.timeout).until(EC.
-                          presence_of_element_located((By.ID, Window.textAddClasses)))
-        self.loadPage()
-        self.click(Window.buttonFinishEnrolling, self.timeout)
-        self.loadPage()
-        self.click(Window.buttonAddAnotherClass, self.timeout)
-
 
     def enroll(self):
         """Checks that cart is not empty, then continue through enrollment screens."""
         self.loadPage()
-        WebDriverWait(self, self.timeout).until(EC.presence_of_element_located((By.ID, Window.buttonEnter)))
+        self.elementPresentWait(Window.buttonEnter, self.timeout)
 
         try:
-            self.find_element_by_id(Window.textClass)
             self.click(Window.buttonStep2of3, self.timeout)
         except TimeoutException:
             self.quit()
@@ -134,7 +130,7 @@ class Window(webdriver.Chrome):
             return 'no'
 
 
-    def enrCheck(self, ceInst):
+    def enrCheck(self):
         """Loop through enrollment screens until cart is empty."""
         try:
             while self.isSuccessful == 'notYet':
@@ -163,8 +159,12 @@ class Window(webdriver.Chrome):
 
     def click(self, id, timeout):
         """Waits for element to load then click it."""
-        WebDriverWait(self, timeout).until(EC.presence_of_element_located((By.ID, id)))
-        self.find_element_by_id(id).click()
+        try:
+            WebDriverWait(self, timeout).until(EC.presence_of_element_located((By.ID, id)))
+            self.find_element_by_id(id).click()
+        except:
+            WebDriverWait(self, timeout).until(EC.presence_of_element_located((By.ID, id)))
+            self.find_element_by_id(id).click()
 
 
     def elementPresent(self, id):
@@ -189,7 +189,7 @@ class Window(webdriver.Chrome):
         """Waits for loading image to disappear."""
         try:
             while True:
-                time.sleep(.025)
+                time.sleep(self.timeout0)
                 self.find_element_by_xpath('//*[@style="display: none; \position: absolute;\
                 right: -205px; z-index: 99991; visibility: visible; top: 196.5px;"]')
         except:
